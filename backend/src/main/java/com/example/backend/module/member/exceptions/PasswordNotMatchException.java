fb00550b6a9b635f483fd9055eebf7e9dc4b22c7
bhdj0107 <bhdj0107@gmail.com>
2025-04-12 17:43:16 +0900

    로그인, 회원가입, 레디스 세션 캐시

---
+++ backend/src/main/java/com/example/backend/module/member/exceptions/PasswordNotMatchException.java
@@ -0,0 +1,7 @@
+package com.example.backend.module.member.exceptions;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class PasswordNotMatchException extends RuntimeException  {
+}
--- backend/src/main/java/com/example/backend/module/member/service/MemberService.java
+++ backend/src/main/java/com/example/backend/module/member/service/MemberService.java
@@ -1,7 +1,61 @@
 package com.example.backend.module.member.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.example.backend.module.member.dto.request.MemberLoginRequest;
+import com.example.backend.module.member.dto.request.MemberSignupRequest;
+import com.example.backend.module.member.entity.Member;
+import com.example.backend.module.member.exceptions.EmailAlreadyExistException;
+import com.example.backend.module.member.exceptions.MemberNotFoundException;
+import com.example.backend.module.member.exceptions.PasswordNotMatchException;
+import com.example.backend.module.member.exceptions.PasswordSpecInvalidException;
+import com.example.backend.module.member.repository.MemberRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void signup(MemberSignupRequest memberRegisterRequest) {
+        Optional<Member> member = memberRepository.findByEmail(memberRegisterRequest.getEmail());
+        if (member.isPresent()) {
+            throw new EmailAlreadyExistException();
+        }
+        if (memberRegisterRequest.getPassword().length() < 8) {
+            throw new PasswordSpecInvalidException();
+        }
+        Member newMember = Member.builder()
+            .email(memberRegisterRequest.getEmail())
+            .password(memberRegisterRequest.getPassword())
+            .build();
+        memberRepository.save(newMember);
+    }
+
+    public void login(MemberLoginRequest memberLoginRequest, HttpServletRequest request) {
+        Optional<Member> member = memberRepository.findByEmail(memberLoginRequest.getEmail());
+        if (member.isEmpty()) {
+            throw new MemberNotFoundException();
+        }
+        else if (!memberLoginRequest.getPassword().equals(member.get().getPassword())) {
+            throw new PasswordNotMatchException();
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute("memberID", member.get().getId());
+        session.setAttribute("memberEmail", member.get().getEmail());
+        return;
+    }
+
+    public void logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        return;
+    }
 }
